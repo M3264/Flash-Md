450a33fc1edc917912deb50a2e7e33db9b381d0c
M3264 <miracle32669@outlook.com>
2024-07-31 08:58:15 +0100

    Update set.js

--- set.js
+++ set.js
@@ -15,6 +15,7 @@ module.exports = { session: process.env.SESSION_ID || 'FLASH-MD-WA-BOT;;;=>eyJub
     AUTO_READ_STATUS: process.env.AUTO_VIEW_STATUS || "on",
 AUTOREAD_MESSAGES: process.env.AUTO_READ_MESSAGES || "off",
 CHATBOT: process.env.CHAT_BOT || "on",
+    PORT: process.env.PORT || "5000",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_SAVE_STATUS || 'off',
     BOT : process.env.BOT_NAME || 'FLASH-MD',
     //OPENAI_API_KEY : process.env.OPENAI_API_KEY || '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',
